f978c17424d383f2e4888fd4e0a508ad5d73a9ca
darkobas <darkobas@gmail.com>
2021-08-21 15:26:23 +0800

    sm8250-common: DeviceSettings: Add DC Dimming toggle & QS tile Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- DeviceSettings/src/org/xtended/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/org/xtended/device/DeviceSettings/DeviceSettings.java
@@ -55,6 +55,7 @@ import org.xtended.device.DeviceSettings.FPSInfoService;
 public class DeviceSettings extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
+    public static final String KEY_DC_SWITCH = "dc";
     public static final String KEY_HBM_SWITCH = "hbm";
     public static final String KEY_AUTO_HBM_SWITCH = "auto_hbm";
     public static final String KEY_AUTO_HBM_THRESHOLD = "auto_hbm_threshold";
@@ -74,6 +75,7 @@ public class DeviceSettings extends PreferenceFragment
     private static SwitchPreference mFpsInfo;
     private static ListPreference mFpsInfoPosition;
     private static ListPreference mFpsInfoColor;
+    private static TwoStatePreference mDCModeSwitch;
     private static TwoStatePreference mHBMModeSwitch;
     private static TwoStatePreference mAutoHBMSwitch;
     private static TwoStatePreference mMuteMedia;
@@ -97,6 +99,11 @@ public class DeviceSettings extends PreferenceFragment
         win.setNavigationBarDividerColor(res.getColor(R.color.primary_color));
 
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDCModeSwitch = (TwoStatePreference) findPreference(KEY_DC_SWITCH);
+        mDCModeSwitch.setEnabled(DCModeSwitch.isSupported());
+        mDCModeSwitch.setChecked(DCModeSwitch.isCurrentlyEnabled(this.getContext()));
+        mDCModeSwitch.setOnPreferenceChangeListener(new DCModeSwitch());
 
         mHBMModeSwitch = (TwoStatePreference) findPreference(KEY_HBM_SWITCH);
         mHBMModeSwitch.setEnabled(HBMModeSwitch.isSupported());
