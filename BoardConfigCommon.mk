d4a559bc9a6c54d527dc64ffa0d56c2f1293d6e1
zahid <zahidhasmani07@gmail.com>
2023-04-18 13:28:37 +0000

    adapt to xtended

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,7 +89,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/evolution/config/device_framework_matrix.xml
+    vendor/xtended/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
@@ -118,7 +118,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
-TARGET_KERNEL_CONFIG := evo_defconfig
+TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
